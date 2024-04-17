af345071a3f5941aeb8e9a92de011914bb0bea69
marcelnoehre <marcelnoehre@web.de>
2024-04-17 10:19:52 +0200

    fix phoca-rename script

--- scripts/mvb/phoca-names.py
+++ scripts/mvb/phoca-names.py
@@ -1,16 +1,17 @@
 import os
 
 def main():
-    year = input('Year:')
-    month = input('Month:')
-    day = input('Day:')
-    event = input('Event:')
+    year = input('Year: ')
+    month = input('Month: ')
+    day = input('Day: ')
+    event = input('Event: ')
     i = 1
     for file in os.listdir('images'):
         if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp')):
-            os.rename(os.path.join('images', file), os.path.join('images', f'{year}-{month}-{day}-{event}-{str(i).zfill(3)}.{file.split('.')[-1]}'))
+            file_extension = file.split('.')[-1]
+            new_filename = f'{year}-{month}-{day}-{event}-{str(i).zfill(3)}.{file_extension}'
+            os.rename(os.path.join('images', file), os.path.join('images', new_filename))
             i += 1
-
 
 if __name__ == '__main__':
     main()
