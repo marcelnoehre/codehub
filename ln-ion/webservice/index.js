67246bcd8ec2a2df1179e92bd47d77bfbc7cd247
marcelnoehre <marcelnoehre@web.de>
2024-04-25 13:19:03 +0200

    store body as json file

--- ln-ion/webservice/index.js
+++ ln-ion/webservice/index.js
@@ -10,8 +10,26 @@ app.use(cors({ origin: '*' }));
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+const fs = require('fs');
+const path = require('path');
+const moment = require('moment');
+
+app.get('/ping', (req, res) => {
     res.send(`Server is running at http://${host}:${port}`);
+});
+
+app.post('/store-file', (req, res, next) => {
+    try {
+        const fileName = `${moment().format('YYYY-MM-DD_HH-mm-ss')}.json`;
+        const filePath = path.resolve(__dirname, 'storage', fileName);
+        const fileContent = JSON.stringify(req.body, null, 2);
+        fs.writeFile(filePath, fileContent, 'utf8', (err) => { 
+            if (err) throw new Error('Error Saving File: ' + err);
+        });
+        res.send('File Saved: ' + fileName);
+    } catch (err) {
+        next(err);
+    }
 });
 
 const swaggerUi = require('swagger-ui-express');
